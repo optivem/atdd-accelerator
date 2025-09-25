e51e50c5c1f3d45b52d9b3eadb620f935c086cf4
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-09-25 13:59:53 +0200

    Fix parameter name in generateNewRepository method for clarity; ensure systemLanguage is passed to the script

--- templategenerator.systemtest/src/test/java/com/optivem/atddaccelerator/templategenerator/systemtest/clients/TemplateGeneratorClient.java
+++ templategenerator.systemtest/src/test/java/com/optivem/atddaccelerator/templategenerator/systemtest/clients/TemplateGeneratorClient.java
@@ -6,8 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TemplateGeneratorClient {
     private static final String SCRIPT_PATH = "../scripts/setup-mono-repo.ps1";
 
-    public void generateNewRepository(String repoName, String java) {
-        var result = executeProcessExpectSuccess("pwsh", SCRIPT_PATH, "-RepositoryName", repoName);
+    public void generateNewRepository(String repoName, String systemLanguage) {
+        executeProcessExpectSuccess("pwsh", SCRIPT_PATH, "-RepositoryName", repoName, "-SystemLanguage", systemLanguage);
     }
 
 }
