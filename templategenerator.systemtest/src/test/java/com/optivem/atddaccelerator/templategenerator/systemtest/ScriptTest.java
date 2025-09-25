e2b542d165657e8a14e4efaa5fedc40fd599cc1b
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-09-25 11:29:07 +0200

    Add ProcessExecutor class for process execution handling

--- templategenerator.systemtest/src/test/java/com/optivem/atddaccelerator/templategenerator/systemtest/ScriptTest.java
+++ templategenerator.systemtest/src/test/java/com/optivem/atddaccelerator/templategenerator/systemtest/ScriptTest.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static com.optivem.atddaccelerator.templategenerator.systemtest.util.ProcessExecutor.execute;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SetupScriptTest {
@@ -17,40 +18,5 @@ class SetupScriptTest {
     void setupScript_shouldReturnExitCode0() throws IOException, InterruptedException {
         var processBuilder = new ProcessBuilder("pwsh", SCRIPT_PATH);
         execute(processBuilder);
-    }
-
-    private static void execute(ProcessBuilder processBuilder) {
-        int exitCode;
-        StringBuilder output = new StringBuilder();
-        StringBuilder errors = new StringBuilder();
-
-        try {
-            Process process = processBuilder.start();
-
-            // Read stdout
-            BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = stdout.readLine()) != null) {
-                output.append(line).append("\n");
-            }
-
-            // Read stderr
-            BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-            while ((line = stderr.readLine()) != null) {
-                errors.append(line).append("\n");
-            }
-
-            exitCode = process.waitFor();
-
-        }catch(Exception e) {
-            throw new RuntimeException("Failed to execute script", e);
-        }
-
-        assertThat(exitCode)
-                .withFailMessage("Exit code is not 0.\nErrors: %s\nOutput: %s", errors.toString(), output.toString())
-                .isZero();
-
     }
 }
---
+++ templategenerator.systemtest/src/test/java/com/optivem/atddaccelerator/templategenerator/systemtest/util/ProcessExecutor.java
@@ -0,0 +1,44 @@
+package com.optivem.atddaccelerator.templategenerator.systemtest.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProcessExecutor {
+
+    public static void execute(ProcessBuilder processBuilder) {
+        int exitCode;
+        StringBuilder output = new StringBuilder();
+        StringBuilder errors = new StringBuilder();
+
+        try {
+            Process process = processBuilder.start();
+
+            // Read stdout
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = stdout.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+
+            // Read stderr
+            BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            while ((line = stderr.readLine()) != null) {
+                errors.append(line).append("\n");
+            }
+
+            exitCode = process.waitFor();
+
+        }catch(Exception e) {
+            throw new RuntimeException("Failed to execute script", e);
+        }
+
+        assertThat(exitCode)
+                .withFailMessage("Exit code is not 0.\nErrors: %s\nOutput: %s", errors.toString(), output.toString())
+                .isZero();
+
+    }
+}
