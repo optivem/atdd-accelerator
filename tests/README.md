f3028f9fe7929f5e66ad849d31ad13e0ba95c4fd
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-10-02 15:33:07 +0200

    Moved folders from console into root, and deleted archived projects

--- tests/README.md
+++
@@ -1,6 +0,0 @@
-﻿# repo-e451de62-67e7-4071-883c-4da57c6cbe89
-
-Generated with ATDD Accelerator
-
-- System Language: typescript
-- System Test Language: typescript
--- tests/src/test/java/com/optivem/atddaccelerator/templategenerator/systemtest/util/WorkflowRunResult.java
+++
@@ -1,9 +0,0 @@
-package com.optivem.atddaccelerator.templategenerator.systemtest.util;
-
-import lombok.Data;
-
-@Data
-public class WorkflowRunResult {
-    private String conclusion;
-    private String status;
-}
